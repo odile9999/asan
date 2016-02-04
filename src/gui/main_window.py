f2879ffec6407d9780910ea86644e8d37c92f0d1
Odile@pc2050 <pc2050@irap>
2016-02-04 16:01:57 +0100

    import

---
+++ src/gui/main_window.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#        Copyright (c) IRAP CNRS
+#        Odile Coeur-Joly, Toulouse, France
+#
+"""
+
+gui.main_window Created on 4 févr. 2016
+"""
+
+
+if __name__ == '__main__':
+    pass
